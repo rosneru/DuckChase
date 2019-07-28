bba8b9b1051259268a6b8f7bcf74684812635751
Uwe Rosner <oscottmusic@posteo.de>
2019-07-28 11:48:01 +0200

    Step-by step building in own code

--- src-cpp/main.cpp
+++ src-cpp/main.cpp
@@ -28,11 +28,11 @@
 #include "GelsBob.h"
 
 
-#define DEPTH 3     //  The number of bitplanes.
+#define DEPTH 3     // The number of view bitplanes
+#define NUMCOLORS 8 // The resulting number of colors
 #define WIDTH 640
-#define HEIGHT 400
+#define HEIGHT 256
 
-void drawFilledBox(WORD, WORD);  /* Function prototypes */
 void cleanup(int);
 void fail(STRPTR);
 
@@ -69,7 +69,6 @@ int main(void)
   // Pointer to old View we can restore it.
   struct View *oldview = NULL;
 
-  WORD depth, box;
   struct RasInfo rasInfo;
   ULONG modeID;
 
@@ -81,120 +80,130 @@ int main(void)
     {VTAG_END_CM, NULL }
   };
 
-  /*  Offsets in BitMap where boxes will be drawn.  */
-  static SHORT boxoffsets[] = { 802, 2010, 3218 };
 
-  // Example steals screen from Intuition if Intuition is around.
-  // Save current View to restore later.
+  // Save current View to restore later
   oldview = GfxBase->ActiView;
 
   LoadView(NULL);
   WaitTOF();
   WaitTOF();
 
-  // Initialize the View and set View.Modes.
+  // Initialize the View and set View.Modes
   InitView(&view);
 
-  /* Form the ModeID from values in <displayinfo.h> */
-  modeID = DEFAULT_MONITOR_ID | HIRESLACE_KEY;
+  // Form the ModeID from values in <displayinfo.h>
+  modeID = DEFAULT_MONITOR_ID | HIRES_KEY;
 
-  /*  Make the ViewExtra structure   */
-  if (vextra = (struct ViewExtra*)GfxNew(VIEW_EXTRA_TYPE))
+  //  Make the ViewExtra structure
+  vextra = (struct ViewExtra*) GfxNew(VIEW_EXTRA_TYPE);
+  if (vextra == NULL)
   {
-    /* Attach the ViewExtra to the View */
-    GfxAssociate(&view, vextra);
-    view.Modes |= EXTEND_VSTRUCT;
-
-    /* Create and attach a MonitorSpec to the ViewExtra */
-    if (monspec = OpenMonitor(NULL, modeID))
-      vextra->Monitor = monspec;
-    else
-      fail("Could not get MonitorSpec\n");
+    fail("Could not get ViewExtra\n");
   }
-  else fail("Could not get ViewExtra\n");
 
+  // Attach the ViewExtra to the View
+  GfxAssociate(&view, vextra);
+  view.Modes |= EXTEND_VSTRUCT;
 
-  /*  Initialize the BitMap for RasInfo.  */
+  // Create and attach a MonitorSpec to the ViewExtra
+  monspec = OpenMonitor(NULL, modeID);
+  if (monspec == NULL)
+  {
+    fail("Could not get MonitorSpec\n");
+  }
+
+  vextra->Monitor = monspec;
+
+  // Initialize the BitMap for RasInfo
   InitBitMap(&bitMap, DEPTH, WIDTH, HEIGHT);
 
-  /* Set the plane pointers to NULL so the cleanup routine */
-  /* will know if they were used.                          */
-  for (depth = 0; depth < DEPTH; depth++)
+  // Set the plane pointers to NULL so the cleanup routine
+  // will know if they were used
+  for (int depth = 0; depth < DEPTH; depth++)
     bitMap.Planes[depth] = NULL;
 
-  /*  Allocate space for BitMap.             */
-  for (depth = 0; depth < DEPTH; depth++)
+  // Allocate space for BitMap
+  for (int depth = 0; depth < DEPTH; depth++)
   {
-    bitMap.Planes[depth] = (PLANEPTR)AllocRaster(WIDTH, HEIGHT);
+    bitMap.Planes[depth] = (PLANEPTR) AllocRaster(WIDTH, HEIGHT);
     if (bitMap.Planes[depth] == NULL)
+    {
       fail("Could not get BitPlanes\n");
+    }
   }
 
   InitRastPort(&rastPort);
   rastPort.BitMap = &bitMap;
   SetRast(&rastPort, 0);
 
-  rasInfo.BitMap = &bitMap;       /*  Initialize the RasInfo.  */
+  // Initialize the RasInfo
+  rasInfo.BitMap = &bitMap;
   rasInfo.RxOffset = 0;
   rasInfo.RyOffset = 0;
   rasInfo.Next = NULL;
 
-  InitVPort(&viewPort);           /*  Initialize the ViewPort.  */
-  view.ViewPort = &viewPort;      /*  Link the ViewPort into the View.  */
+  // Initialize the ViewPort
+  InitVPort(&viewPort);
+  view.ViewPort = &viewPort; // Link the ViewPort into the View
   viewPort.RasInfo = &rasInfo;
   viewPort.DWidth = WIDTH;
   viewPort.DHeight = HEIGHT;
 
-  /* Set the display mode the old-fashioned way */
-  viewPort.Modes = HIRES;
+  // Make a ViewPortExtra and get ready to attach it
+  vpextra = (struct ViewPortExtra*) GfxNew(VIEWPORT_EXTRA_TYPE);
 
-  /* Make a ViewPortExtra and get ready to attach it */
-  if (vpextra = (struct ViewPortExtra*)GfxNew(VIEWPORT_EXTRA_TYPE))
+  if (vpextra == NULL)
   {
-    vcTags[1].ti_Data = (ULONG)vpextra;
-
-    /* Initialize the DisplayClip field of the ViewPortExtra */
-    if (GetDisplayInfoData(NULL, (UBYTE *)&dimquery,
-      sizeof(dimquery), DTAG_DIMS, modeID))
-    {
-      vpextra->DisplayClip = dimquery.Nominal;
-
-      /* Make a DisplayInfo and get ready to attach it */
-      if (!(vcTags[2].ti_Data = (ULONG)FindDisplayInfo(modeID)))
-        fail("Could not get DisplayInfo\n");
-    }
-    else fail("Could not get DimensionInfo \n");
+    fail("Could not get ViewPortExtra\n");
   }
-  else fail("Could not get ViewPortExtra\n");
 
-  /*  Initialize the ColorMap.  */
-  /*  2 planes deep, so 4 entries (2 raised to the #_planes power).  */
-  cm = GetColorMap(4L);
+  vcTags[1].ti_Data = (ULONG)vpextra;
+
+  // Initialize the DisplayClip field of the ViewPortExtra
+  if (GetDisplayInfoData(NULL, (UBYTE *)&dimquery,
+    sizeof(dimquery), DTAG_DIMS, modeID) == 0)
+  {
+    fail("Could not get DimensionInfo \n");
+  }
+
+  vpextra->DisplayClip = dimquery.Nominal;
+
+  // Make a DisplayInfo and get ready to attach it
+  if (!(vcTags[2].ti_Data = (ULONG) FindDisplayInfo(modeID)))
+  {
+    fail("Could not get DisplayInfo\n");
+  }
+
+
+
+  // Initialize the ColorMap, 3 planes deep, so 8 entries
+  cm = GetColorMap(NUMCOLORS);
   if (cm == NULL)
-    fail("Could not get ColorMap\n");
-
-  if (GfxBase->LibNode.lib_Version >= 36)
   {
-    /* Get ready to attach the ColorMap, Release 2-style */
-    vcTags[0].ti_Data = (ULONG)&viewPort;
-
-    /* Attach the color map and Release 2 extended structures */
-    if (VideoControl(cm, vcTags))
-      fail("Could not attach extended structures\n");
+    fail("Could not get ColorMap\n");
   }
-  else
-    /* Attach the ColorMap, old 1.3-style */
-    viewPort.ColorMap = cm;
+
+  /* Get ready to attach the ColorMap, Release 2-style */
+  vcTags[0].ti_Data = (ULONG)&viewPort;
+
+  /* Attach the color map and Release 2 extended structures */
+  if (VideoControl(cm, vcTags))
+  {
+    fail("Could not attach extended structures\n");
+  }
+
 
   //
   // Setting the used color table (extracted from pic wit BtoC32)
   //
+
   USHORT colortable[8] = {
     0xAAA, 0x0, 0xFFF, 0x68B, 0x5A3, 0xEB0, 0xB52, 0xF80
   };
 
   // Change colors to those in colortable
-  LoadRGB4(&viewPort, colortable, 8);
+  LoadRGB4(&viewPort, colortable, NUMCOLORS);
+
 
   MakeVPort(&view, &viewPort); /* Construct preliminary Copper instruction list.    */
 
@@ -202,38 +211,26 @@ int main(void)
   MrgCop(&view);
 
   /* Clear the ViewPort */
-  for (depth = 0; depth < DEPTH; depth++)
+  for (int depth = 0; depth < DEPTH; depth++)
   {
     displaymem = (UBYTE *)bitMap.Planes[depth];
     BltClear(displaymem, (bitMap.BytesPerRow * bitMap.Rows), 1L);
   }
 
   LoadView(&view);
-/*
-  //  Now fill some boxes so that user can see something.
-  //  Always draw into both planes to assure true colors.
-  for (box = 1; box <= 3; box++)  // Three boxes; red, green and blue.
-  {
-    for (depth = 0; depth < DEPTH; depth++)
-    {
-      displaymem = bitMap.Planes[depth] + boxoffsets[box - 1];
-      drawFilledBox(box, depth);
-    }
-  }
-*/
 
   //
   // Loading background image
   //
   Picture picBackgr;
   if(picBackgr.LoadFromRawFile("/gfx/background_hires.raw",
-                               640, 256, 3) == FALSE)
+                               WIDTH, HEIGHT, DEPTH) == FALSE)
   {
     fail("Could not load background image");
   }
 
   BltBitMapRastPort(picBackgr.GetBitMap(), 0, 0, &rastPort,
-                    0, 0, 640, 256, 0xC0);
+                    0, 0, WIDTH, HEIGHT, 0xC0);
 
 
 
@@ -270,8 +267,6 @@ void fail(STRPTR errorstring)
  */
 void cleanup(int returncode)
 {
-  WORD depth;
-
   /*  Free the color map created by GetColorMap().  */
   if (cm) FreeColorMap(cm);
 
@@ -279,7 +274,7 @@ void cleanup(int returncode)
   if (vpextra) GfxFree(vpextra);
 
   /*  Free the BitPlanes drawing area.  */
-  for (depth = 0; depth < DEPTH; depth++)
+  for (int depth = 0; depth < DEPTH; depth++)
   {
     if (bitMap.Planes[depth])
       FreeRaster(bitMap.Planes[depth], WIDTH, HEIGHT);
@@ -295,30 +290,4 @@ void cleanup(int returncode)
                 TAG_END);
 
   exit(returncode);
-}
-
-
-/*
- * drawFilledBox(): create a WIDTH/2 by HEIGHT/2 box of color
- *                  "fillcolor" into the given plane.
- */
-void drawFilledBox(WORD fillcolor, WORD plane)
-{
-  UBYTE value;
-  WORD boxHeight, boxWidth, width;
-
-  /*  Divide (WIDTH/2) by eight because each UBYTE that */
-  /* is written stuffs eight bits into the BitMap.      */
-  boxWidth = (WIDTH / 2) / 8;
-  boxHeight = HEIGHT / 2;
-
-  value = ((fillcolor & (1 << plane)) != 0) ? 0xff : 0x00;
-
-  for (; boxHeight; boxHeight--)
-  {
-    for (width = 0; width < boxWidth; width++)
-      *displaymem++ = value;
-
-    displaymem += (bitMap.BytesPerRow - boxWidth);
-  }
 }
