136296cc8cb79e13a9934199edb0448fe975b3af
Uwe Rosner <uwe.rosner@difitec.de>
2019-07-29 16:31:39 +0200

    Added first members to GameView

--- src-cpp/GameView.cpp
+++ src-cpp/GameView.cpp
@@ -1,8 +1,19 @@
 #include "GameView.h"
 
 
-GameView::GameView()
-  : 
+GameView::GameView(short viewWidth, short viewHeight, short viewDepth)
+  : m_ViewWidth(viewWidth),
+    m_ViewHeight(viewHeight),
+    m_ViewDepth(viewDepth),
+    viewPort(0),
+    bitMap1(0),
+    bitMap2(0),
+    pBitMap(NULL),
+    cm(NULL),
+    vextra(NULL),
+    monspec(NULL),
+    vpextra(NULL),
+    dimquery(0)
 {
 }
 
@@ -10,4 +21,9 @@ GameView::GameView()
 GameView::~GameView()
 {
 
+}
+
+bool GameView::Init()
+{
+  return false;
 }
--- src-cpp/GameView.h
+++ src-cpp/GameView.h
@@ -1,6 +1,15 @@
 #ifndef GAME_VIEW_H
 #define GAME_VIEW_H
 
+#include <graphics/gfx.h>
+#include <graphics/gfxbase.h>
+#include <graphics/gfxmacros.h>
+#include <graphics/copper.h>
+#include <graphics/view.h>
+#include <graphics/displayinfo.h>
+#include <graphics/gfxnodes.h>
+#include <graphics/videocontrol.h>
+
 
 /**
  * Represents a graphics.library constructed, double buffered view
@@ -12,13 +21,29 @@
 class GameView
 {
 public:
-  GameView();
+  GameView(short viewWidth, short viewHeight, short viewDepth);
   ~GameView();
+  
+  bool Init();
 
 private:
+  short m_ViewWidth;
+  short m_ViewHeight;
+  short m_ViewDepth;
 
+  // Basic structures as of Release 1.3
+  struct View view;
+  struct ViewPort viewPort;
+  struct BitMap bitMap1;
+  struct BitMap bitMap2;
+  struct BitMap* pBitMap; // TODO remove from GameView to GameLoop ?
+  struct ColorMap *cm = NULL;
 
-  void clear();
+  // Extended structures used in Release 2
+  struct ViewExtra *vextra = NULL;
+  struct MonitorSpec *monspec = NULL;
+  struct ViewPortExtra *vpextra = NULL;
+  struct DimensionInfo dimquery;
 };
 
 #endif
