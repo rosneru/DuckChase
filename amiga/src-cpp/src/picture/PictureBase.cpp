c61c68b46e8cd861609ad00ca7491c75f8f7c6c5
Uwe Rosner <oscottmusic@posteo.de>
2020-06-12 19:02:53 +0200

    Removed obsolete includes

--- amiga/src-cpp/src/picture/PictureBase.cpp
+++ amiga/src-cpp/src/picture/PictureBase.cpp
@@ -1,10 +1,3 @@
-#include <clib/dos_protos.h>
-#include <clib/exec_protos.h>
-#include <dos/dos.h>
-#include <exec/memory.h>
-
-#include <string.h>
-
 #include "PictureBase.h"
 
 PictureBase::PictureBase()
