b65543a7ecdaee2e4367e5a54c8e37392bc6bb12
Uwe Rosner <oscottmusic@posteo.de>
2020-06-24 20:09:53 +0200

    Copy'n'pasted all needed 8SVX structs from COmmodore/newiff code

--- amiga/src-cpp/src/sound/Soundfile8SVX.cpp
+++ amiga/src-cpp/src/sound/Soundfile8SVX.cpp
@@ -1,8 +1,9 @@
+#include <stddef.h>
 #include "SoundFile8SVX.h"
 
 Soundfile8SVX::Soundfile8SVX(const char* pFileName)
 {
-
+  struct EightSVXInfo  *esvx = NULL;
 }
 
 Soundfile8SVX::~Soundfile8SVX()
